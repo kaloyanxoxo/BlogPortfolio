f2f6a2b6fa2957734e835af0652e153d4c649679
Kaloyan Kostadinov <Kaloyan@Kaloyans-Pro.local>
2018-02-19 01:00:09 +0200

    Added validation to caterogy's model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,3 @@
 class Category < ApplicationRecord
-	validates :title, presence: true
+	validates :title, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 4 , maximum: 25 }
 end
